649507c466a2a50dc5ff556a124abb023123bbd4
Mario Romano <dnamario@hotmail.com>
2018-06-06 09:31:45 +0100

    update timeout tests

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -30,7 +30,8 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox'],
+        timeout: 2000000
       }
     },
     singleRun: true
