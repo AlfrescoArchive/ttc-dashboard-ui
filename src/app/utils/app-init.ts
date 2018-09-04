14b125c71219b64fde38a7a08617168ef42eca20
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2018-09-04 09:46:22 +0100

    app-init.ts initialize method updated

--- src/app/utils/app-init.ts
+++ src/app/utils/app-init.ts
@@ -4,7 +4,22 @@ export function initializer(keycloak: KeycloakService): () => Promise<any> {
   return (): Promise<any> => {
     return new Promise(async (resolve, reject) => {
       try {
-        await keycloak.init();
+        await keycloak.init({
+            config: {
+              url: 'http://activiti-keycloak.jx-staging.35.189.236.129.nip.io/auth/',
+              realm: 'activiti',
+              clientId: 'activiti'
+            },
+            initOptions: {
+              onLoad: 'login-required',
+              checkLoginIframe: false
+            },
+            enableBearerInterceptor: true,
+            bearerExcludedUrls: [
+              '/assets',
+              '/clients/public'
+            ],
+          });
         resolve();
       } catch (error) {
         reject(error);
