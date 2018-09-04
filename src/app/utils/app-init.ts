55c71e73a0104f6203cea1fb0cfbf413551ce1b8
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2018-09-04 10:11:31 +0100

    app-init.ts keycloak config outsourced to environments

--- src/app/utils/app-init.ts
+++ src/app/utils/app-init.ts
@@ -1,23 +1,20 @@
 import { KeycloakService } from 'keycloak-angular';
+import { environment } from '../../environments/environment';
 
 export function initializer(keycloak: KeycloakService): () => Promise<any> {
   return (): Promise<any> => {
     return new Promise(async (resolve, reject) => {
       try {
         await keycloak.init({
-            config: {
-              url: 'http://activiti-keycloak.jx-staging.35.189.236.129.nip.io/auth/',
-              realm: 'activiti',
-              clientId: 'activiti'
-            },
+            config: environment.keycloak,
             initOptions: {
               onLoad: 'login-required',
               checkLoginIframe: false
             },
             enableBearerInterceptor: true,
             bearerExcludedUrls: [
-              '/assets',
-              '/clients/public'
+            //   '/assets',
+            //   '/clients/public'
             ],
           });
         resolve();
