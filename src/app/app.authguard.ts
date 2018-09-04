fac8ec5db1ba7e460d6171c036063c0d1aa5378d
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2018-09-04 09:59:45 +0100

    app-routing.module.ts modified

--- src/app/app.authguard.ts
+++ src/app/app.authguard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { KeycloakService, KeycloakAuthGuard } from 'keycloak-angular';
 
 @Injectable()
