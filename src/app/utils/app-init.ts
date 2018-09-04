af4763feec07940f8a49f502d47fee06aa02ac06
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2018-09-04 09:39:01 +0100

    utils/app-init.ts created and app.module modified accordingly

---
+++ src/app/utils/app-init.ts
@@ -0,0 +1,14 @@
+import { KeycloakService } from 'keycloak-angular';
+
+export function initializer(keycloak: KeycloakService): () => Promise<any> {
+  return (): Promise<any> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await keycloak.init();
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+}
